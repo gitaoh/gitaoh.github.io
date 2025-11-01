dbcf332098e7e7a4bc2a1b3b98db83d8e5ae965e
gitaoh <233471135+gitaoh@users.noreply.github.com>
2025-11-01 12:07:18 +0300

    update Next.js configuration to enable trailing slashes and set asset and base paths

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
 import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-    /* config options here */
     reactCompiler: true,
     output: "export",
-    trailingSlash: true,
-    assetPrefix: "/",
-    basePath: "/",
     images: {
         unoptimized: true
     }

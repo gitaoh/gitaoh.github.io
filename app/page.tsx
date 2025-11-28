718616bd06ec9b362394af2c8bc0da86fe9d1596
gitaoh <code.gitaoh@gmail.com>
2025-11-28 17:12:13 +0300

    :rocket: Modified Workflows

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,24 @@
+// "use client"
+
+// import {useRef} from 'react';
+
+// import gsap from 'gsap';
+// import {useGSAP} from '@gsap/react';
+//
+// gsap.registerPlugin(useGSAP);
+
+
 export default function Home() {
+    // const container = useRef(null);
+    //
+    // useGSAP(() => {
+    //     gsap.to('.box', {rotation: 180});
+    // }, {scope: container});
     return (
-        <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans">
-            <main
-                className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white  sm:items-start">
-                <h1 className={"text-4xl font-extrabold sm:text-5xl md:text-6xl dark:text-gray-700 text-gray-100"}>Super
-                    Cool Website built by <span><a
-                        href="https://github.com/gitaoh"
-                        className={"text-blue-500 hover:text-blue-500 hover:underline dark:text-blue-700"}>@gitaoh</a></span>
-                </h1>
-            </main>
+        <div className="flex min-h-screen items-center justify-center bg-white font-sans">
+            <h1 className={"text-9xl font-extrabold text-cyan-900"}>
+                HELLO WORLD
+            </h1>
         </div>
     );
 }
